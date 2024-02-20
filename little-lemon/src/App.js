843f0a80beb3964c4160af95a8b0655845853098
HoundSL <86515184+janindu-kulathilaka@users.noreply.github.com>
2024-02-20 12:49:08 +0530

    add the pages

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
-import Footer from './components/Footer';
-import Main from './components/Main';
-import Nav from './components/Nav';
+import Home from './pages/HomePage';
+import About from './pages/AboutPage';
+import Menu from './pages/MenuPage';
+import Reservations from './pages/ReservationsPage';
+import OrderOnline from './pages/OrderOnlinePage';
+import Login from './pages/LoginPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Main />
-      <Footer />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/reservations" element={<Reservations />} />
+        <Route path="/order-online" element={<OrderOnline />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ little-lemon/src/pages/OrderOnlinePage.js
@@ -0,0 +1,7 @@
+export default function OrderOnlinePage() {
+  return (
+    <div>
+      <h1>Order Online</h1>
+    </div>
+  );
+}
