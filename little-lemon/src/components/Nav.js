1070db01fc9f346a26e28c4f7972f6f78801d82d
HoundSL <86515184+janindu-kulathilaka@users.noreply.github.com>
2024-02-20 11:45:48 +0530

    creat the navbar

--- little-lemon/src/components/Nav.js
+++ little-lemon/src/components/Nav.js
@@ -1,7 +1,21 @@
+import React from 'react';
+import Logo from '../assets/logo.png';
+import '../css/Nav.css';
+
 export default function Nav() {
   return (
-    <>
-      <h1>Little Lemon Navbar</h1>
-    </>
+    <div className="navbar">
+      <img src={Logo} alt="little lemon logo" className="logo" />
+      <nav>
+        <ul className="nav">
+          <li>HOME</li>
+          <li>ABOUT</li>
+          <li>MENU</li>
+          <li>RESERVATIONS</li>
+          <li>ORDER ONLINE</li>
+          <li>LOGIN</li>
+        </ul>
+      </nav>
+    </div>
   );
 }
